5bb7bfe17da11f3681ecfd5271d2ef1173f8d229
Sergey Sokolov <xxxARGUSxxx58@gmail.com>
2022-08-30 11:47:05 +0500

    rename dto

--- src/modules/topic/dto/index.ts
+++ src/modules/topic/dto/index.ts
@@ -1,5 +1,5 @@
 export * from "./topic.create.dto";
 export * from "./topic.dto";
-export * from "./topic.idDto";
+export * from "./topic.id.dto";
 export * from "./topic.toQuestion.dto";
 export * from "./topic.update.dto";
