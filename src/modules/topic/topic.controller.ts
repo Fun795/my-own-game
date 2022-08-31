0af977a83cee7841a4283385caff5733e4f04a3e
Sergey Sokolov <xxxARGUSxxx58@gmail.com>
2022-08-31 12:52:39 +0500

    Update topic.controller.ts

--- src/modules/topic/topic.controller.ts
+++ src/modules/topic/topic.controller.ts
@@ -1,8 +1,16 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
 import { TopicService } from "./topic.service";
 import { TopicReateDto, TopicUpdateDto, TopicToQuestionDto } from "./dto";
-import { ApiTags } from "@nestjs/swagger";
+import {
+    ApiBadRequestResponse,
+    ApiCreatedResponse,
+    ApiNotFoundResponse,
+    ApiOkResponse,
+    ApiOperation,
+    ApiTags
+} from "@nestjs/swagger";
 import { Topic } from "./entities/topic.entity";
+import { Question } from "../question/question.entity";
 
 @ApiTags("topic")
 @Controller("topic")
@@ -15,26 +23,47 @@ export class TopicController {
     }
 
     @Get(":id")
+    @ApiNotFoundResponse({
+        description: "Topic not found by number id"
+    })
+    @ApiOkResponse()
     findOne(@Param("id") id: number) {
         return this.topicService.findOne(id);
     }
 
     @Post()
+    @ApiBadRequestResponse({
+        description: "Invalid body params"
+    })
+    @ApiCreatedResponse({
+        description: "Topic created",
+        type: Topic
+    })
     create(@Query() createTopicDto: TopicReateDto) {
         return this.topicService.create(createTopicDto);
     }
 
     @Patch()
+    @ApiNotFoundResponse({
+        description: "Topic not found by number id"
+    })
+    @ApiOkResponse({ description: "Success update" })
     update(@Body() updateTopicDto: TopicUpdateDto) {
         return this.topicService.update(updateTopicDto);
     }
 
     @Delete(":id")
+    @ApiOperation({ operationId: "removeQuestion" })
+    @ApiNotFoundResponse({
+        description: "Topic not found by number id"
+    })
+    @ApiOkResponse({ description: "Success remove" })
     remove(@Param("id") id: number) {
         return this.topicService.remove(id);
     }
 
     @Post("findAllManyTopic")
+    @ApiOkResponse()
     findAllManyTopic(): Promise<Topic[]> {
         return this.topicService.findAllManyTopic();
     }
